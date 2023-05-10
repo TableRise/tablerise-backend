dc95cb5e702146ccc9c24198959b1d80c663f632
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-05-10 10:13:35 -0300

    feat: Create interface for attack

---
+++ src/interfaces/IAttack.ts
@@ -0,0 +1,46 @@
+import { z } from 'zod';
+
+const attackZodSchema = z.object({
+  system_id: z.string(),
+  name: z.string(),
+  range: z.string(),
+  type: z.enum([
+    'fisíco',
+    'mágico'
+  ]),
+  damage: z.string(),
+  damage_type: z.enum([
+    'Perfurante',
+    'Cortante',
+    'Contundente',
+    'Fogo',
+    'Gelo',
+    'Elétrico',
+    'Ácido',
+    'Venenoso',
+    'Psíquico',
+    'Radiante',
+    'Necrótico',
+    'Força',
+    'Sônico',
+    'Fome',
+    'Luz Solar',
+    'Trovão',
+    'Vácuo',
+    'Queda',
+    'Ruptura',
+    'Maldição',
+    'Dreno',
+  ]),
+  tags: z.array(z.string()).optional(),
+  description: z.string(),
+  buffs: z.array(z.string()),
+  debuffs: z.array(z.string()),
+  requirements: z.string(),
+  duration: z.string(),
+  conjuration: z.string(),
+});
+
+type IAttack = z.infer<typeof attackZodSchema>;
+
+export { IAttack, attackZodSchema };
