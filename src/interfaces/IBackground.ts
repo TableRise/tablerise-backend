1f74ec11525e1518f7891a76798f6447680b4d44
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-05-14 11:57:06 -0300

    feat: Create interface for background

---
+++ src/interfaces/IBackground.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+const backgroundZodSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  bonus: z.object({
+    name: z.string(),
+    value: z.number(),
+  }),
+  equipment:  z.object({
+    name: z.string(),
+    damage: z.string(),
+  })
+});
+
+type IBackground = z.infer<typeof backgroundZodSchema>;
+
+export { IBackground, backgroundZodSchema };
