19a5b9397a42a83d67621087bc98e631be717297
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-05-17 17:34:40 -0300

    setup: Change import paths to absolute

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['build']
+  modulePathIgnorePatterns: ['build'],
+  rootDir: './',
+  modulePaths: ['<rootDir>']
 }
