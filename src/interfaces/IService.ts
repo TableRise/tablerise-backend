2ba8abe461401ece7f2fadb7e34fe7fca0a36995
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-06-07 19:43:04 -0300

    ref: Remove interfaces

--- src/interfaces/IService.ts
+++
@@ -1,9 +0,0 @@
-interface IService<T> {
-  create: (payload: T) => Promise<T>
-  findAll: () => Promise<T[]>
-  findOne: (_id: string) => Promise<T | null>
-  update: (_id: string, payload: T) => Promise<T | null>
-  delete: (_id: string) => Promise<T | null>
-}
-
-export default IService;
