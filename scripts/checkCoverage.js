e182e59b45dbee0bf2873ef52babaefb44fd2d73
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-06-25 11:07:43 -0300

    feat: Create script and actions for check code test coverage

---
+++ scripts/checkCoverage.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+const summary = require('../coverage/coverage-summary.json');
+
+function checkCoverage() {
+  const summaryParsed = JSON.parse(summary);
+  const summaryTotal = summaryParsed.total;
+
+  const summaryPercentages = [
+    summaryTotal.lines.pct,
+    summaryTotal.statements.pct,
+    summaryTotal.functions.pct,
+    summaryTotal.branches.pct
+  ];
+
+  summaryPercentages.forEach((spec) => {
+    if (spec < 90) {
+      throw new Error('Some coverage point is below 90%');
+    }
+
+    console.log('Well Done, all the coverage point are above or equal 90%');
+  });
+}
+
+checkCoverage();
