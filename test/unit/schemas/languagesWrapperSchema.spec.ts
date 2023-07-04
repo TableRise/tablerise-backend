3d3c4c6ca9a43243d4e1882fafd7193e346dca50
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-07-04 15:31:49 -0300

    feat: Create wrapper to add language support to schemas

---
+++ test/unit/schemas/languagesWrapperSchema.spec.ts
@@ -0,0 +1,24 @@
+import languagesWrapperSchema from 'src/schemas/languagesWrapperSchema';
+import godZodSchema from 'src/schemas/godsValidationSchema';
+import mocks from 'src/support/mocks';
+
+describe('Schemas :: LanguagesWrapperSchema', () => {
+  describe('When the zod validation is called with the correct data', () => {
+    it('should be successfull', () => {
+      const schemaValidation = languagesWrapperSchema(godZodSchema).safeParse({
+        en: mocks.god.instance,
+        pt: mocks.god.instance
+      });
+      expect(schemaValidation.success).toBe(true);
+    });
+  });
+
+  describe('When the zod validation is called with the incorrect data', () => {
+    it('should fail', () => {
+      const schemaValidation = languagesWrapperSchema(godZodSchema).safeParse({
+        en: mocks.god.instance
+      });
+      expect(schemaValidation.success).toBe(false);
+    });
+  });
+});
