e83a5a7ec2f06152396610616066f5e69c3c87d8
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-07-09 10:20:11 -0300

    ref: Change environments of database

--- src/server.ts
+++ src/server.ts
@@ -8,13 +8,12 @@ const port = process.env.PORT || 3001;
 
 const MONGODB_USERNAME = process.env.MONGODB_USERNAME || 'root';
 const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD || 'secret';
-const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
-const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
+const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost:27017';
 const MONGODB_DATABASE = process.env.MONGODB_DATABASE || 'tavern-data?authSource=admin';
 const MONGODB_CONNECTION_INITIAL = process.env.MONGODB_CONNECTION_INITIAL || 'mongodb';
 
 mongoose.connect(`${MONGODB_CONNECTION_INITIAL}://${MONGODB_USERNAME}:
-${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`)
+${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_DATABASE}`)
   .then(() => { console.log('MongoDB connected successfully'); })
   .catch((err) => { console.log(err); });
 
