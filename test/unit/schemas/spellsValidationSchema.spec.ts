432a439047676b1b06efa2ca4115249db729235d
Cesar Holanda <rasecmh0@gmail.com>
2023-07-10 21:03:48 -0300

    test: create spells schema tests.

---
+++ test/unit/schemas/spellsValidationSchema.spec.ts
@@ -0,0 +1,20 @@
+import spellZodSchema, { Spell } from 'src/schemas/spellsValidationSchema';
+import mocks from 'src/support/mocks';
+
+describe('Schemas :: spellsValidationSchema', () => {
+  describe('When the zod validation is called with the correct data', () => {
+    it('should be successfull', () => {
+      const schemaValidation = spellZodSchema.safeParse(mocks.spell.instance.en);
+      expect(schemaValidation.success).toBe(true);
+    });
+  });
+
+  describe('When the zod validation is called with the incorrect data', () => {
+    it('should fail', () => {
+      const { name: _, ...spellWithoutName } = mocks.spell.instance.en as Spell;
+
+      const schemaValidation = spellZodSchema.safeParse(spellWithoutName);
+      expect(schemaValidation.success).toBe(false);
+    });
+  });
+});
