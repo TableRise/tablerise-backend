410c9db4b76920308625ec956f58b19492a81c3b
Cesar Holanda <rasecmh0@gmail.com>
2023-07-11 22:17:25 -0300

    test: create classes validation tests

---
+++ test/unit/schemas/classesValidationSchema.spec.ts
@@ -0,0 +1,20 @@
+import classZodSchema, { Class } from 'src/schemas/classesValidationSchema';
+import mocks from 'src/support/mocks';
+
+describe('Schemas :: classesValidationSchema', () => {
+  describe('When the zod validation is called with the correct data', () => {
+    it('should be successfull', () => {
+      const schemaValidation = classZodSchema.safeParse(mocks.class.instance.en);
+      expect(schemaValidation.success).toBe(true);
+    });
+  });
+
+  describe('When the zod validation is called with the incorrect data', () => {
+    it('should fail', () => {
+      const { name: _, ...classWithoutName } = mocks.class.instance.en as Class;
+
+      const schemaValidation = classZodSchema.safeParse(classWithoutName);
+      expect(schemaValidation.success).toBe(false);
+    });
+  });
+});
