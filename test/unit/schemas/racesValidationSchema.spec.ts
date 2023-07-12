797335c770647bd6c6bd7acc2035e07e1b81d4ad
junglejf <junglejf@gmail.com>
2023-07-12 10:25:33 -0300

    test/34/create_test_for_races

--- test/unit/schemas/racesValidationSchema.spec.ts
+++ test/unit/schemas/racesValidationSchema.spec.ts
@@ -1,19 +1,19 @@
-import itensZodSchema, { Item } from 'src/schemas/itensValidationSchema';
+import racesZodSchema, { Race } from 'src/schemas/racesValidationSchema';
 import mocks from 'src/support/mocks';
 
-describe('Schemas :: ItensValidationSchema', () => {
+describe('Schemas :: RacesValidationSchema', () => {
   describe('When the zod validation is called with the correct data', () => {
     it('should be successfull', () => {
-      const schemaValidation = itensZodSchema.safeParse(mocks.item.instance.en);
+      const schemaValidation = racesZodSchema.safeParse(mocks.race.instance.en);
       expect(schemaValidation.success).toBe(true);
     });
   });
 
   describe('When the zod validation is called with the incorrect data', () => {
     it('should fail', () => {
-      const { name: _, ...godMockWithoutName } = mocks.item.instance.en as Item;
+      const { name: _, ...godMockWithoutName } = mocks.race.instance.en as Race;
 
-      const schemaValidation = itensZodSchema.safeParse(godMockWithoutName);
+      const schemaValidation = racesZodSchema.safeParse(godMockWithoutName);
       expect(schemaValidation.success).toBe(false);
     });
   });
