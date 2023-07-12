a34dcaf567471c01835255eaed6dc8dfc993e9e7
Cesar Holanda <rasecmh0@gmail.com>
2023-07-11 22:16:31 -0300

    feat: create classes validation mock

--- src/support/mocks/index.ts
+++ src/support/mocks/index.ts
@@ -5,6 +5,7 @@ import backgroundMocks from 'src/support/mocks/backgroundsMocks';
 import featMocks from 'src/support/mocks/featsMocks';
 import Mock from 'src/types/Mock';
 import spellMocks from 'src/support/mocks/spellsMocks';
+import classMocks from 'src/support/mocks/classesMocks';
 
 export default {
   system: systemsMocks,
@@ -13,5 +14,6 @@ export default {
   god: godsMocks,
   background: backgroundMocks,
   feat: featMocks,
-  spell: spellMocks
+  spell: spellMocks,
+  class: classMocks
 }
