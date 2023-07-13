8e19e1db8ce9b43cf2dc8ea16b8608fc37fe1b3d
Cesar Holanda <rasecmh0@gmail.com>
2023-07-13 18:12:38 -0300

    test: create armos schema tests

---
+++ test/unit/schemas/armorsValidationSchema.spec.ts
@@ -0,0 +1,20 @@
+import armorZodSchema, { Armor } from 'src/schemas/armorsValidationSchema';
+import mocks from 'src/support/mocks';
+
+describe('Schemas :: armorsValidationSchema', () => {
+  describe('When the zod validation is called with the correct data', () => {
+    it('should be successfull', () => {
+      const schemaValidation = armorZodSchema.safeParse(mocks.armor.instance.en);
+      expect(schemaValidation.success).toBe(true);
+    });
+  });
+
+  describe('When the zod validation is called with the incorrect data', () => {
+    it('should fail', () => {
+      const { name: _, ...armorWithoutName } = mocks.armor.instance.en as Armor;
+
+      const schemaValidation = armorZodSchema.safeParse(armorWithoutName);
+      expect(schemaValidation.success).toBe(false);
+    });
+  });
+});
