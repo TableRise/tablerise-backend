6f963fd0062491559b47ba20372419f7878216b4
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-07-20 16:22:42 -0300

    feat: Add new fields to items schema

--- src/schemas/itemsValidationSchema.ts
+++ src/schemas/itemsValidationSchema.ts
@@ -1,11 +1,24 @@
 import { z } from 'zod';
 
+const tradeGoodsZodSchema = z.object({
+  isValid: z.boolean(),
+  goods: z.string()
+});
+
+const mountOrVehicleZodSchema = z.object({
+  isValid: z.boolean(),
+  speed: z.string(),
+  carryingCapacity: z.string()
+});
+
 const itemsZodSchema = z.object({
   name: z.string(),
   description: z.string(),
   cost: z.number(),
   type: z.string(),
-  weight: z.number()
+  weight: z.number(),
+  mountOrVehicle: mountOrVehicleZodSchema,
+  tradeGoods: tradeGoodsZodSchema
 });
 
 export type Item = z.infer<typeof itemsZodSchema>;
