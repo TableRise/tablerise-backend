b792a6bd5f9a0a1697b86ab88ea45ad2681c0fa3
Cesar Holanda <rasecmh0@gmail.com>
2023-08-01 18:40:22 -0300

    ref: remove unnecessary schema

--- src/schemas/updateAvailabilitySchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-const updateAvailabilityZodSchema = z.object({
-    active: z.boolean(),
-});
-
-export type UpdateAvailability = z.infer<typeof updateAvailabilityZodSchema>;
-export default updateAvailabilityZodSchema;
