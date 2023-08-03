5a23a09c2654cdaec9a0ac263b5bfacbfc18c7dc
Cesar Holanda <rasecmh0@gmail.com>
2023-08-03 15:45:14 -0300

    feat: add new update and get routes for armors

--- src/routes/armorRoutes.ts
+++ src/routes/armorRoutes.ts
@@ -3,6 +3,7 @@ import ArmorsModel from 'src/database/models/ArmorsModel';
 import ArmorsServices from 'src/services/ArmorsServices';
 import ArmorsControllers from 'src/controllers/ArmorsControllers';
 import VerifyIdMiddleware from 'src/middlewares/VerifyIdMiddleware';
+import VerifyBooleanQueryMiddleware from 'src/middlewares/VerifyBooleanQueryMiddleware';
 
 const logger = require('@tablerise/dynamic-logger');
 
@@ -13,8 +14,9 @@ const controllers = new ArmorsControllers(services, logger);
 const router = Router();
 
 router.get('/', controllers.findAll);
+router.get('/disabled', controllers.findAllDisabled);
 router.get('/:id', VerifyIdMiddleware, controllers.findOne);
 router.put('/:id', VerifyIdMiddleware, controllers.update);
-router.delete('/:id', VerifyIdMiddleware, controllers.delete);
+router.patch('/:id', VerifyIdMiddleware, VerifyBooleanQueryMiddleware, controllers.updateAvailability);
 
 export default router;
--- src/services/ArmorsServices.ts
+++ src/services/ArmorsServices.ts
@@ -1,10 +1,11 @@
 import ArmorsModel from 'src/database/models/ArmorsModel';
 import Service from 'src/types/Service';
-import armorZodSchema, { Armor } from 'src/schemas/armorsValidationSchema';
+import armorsZodSchema, { Armor } from 'src/schemas/armorsValidationSchema';
 import languagesWrapper, { Internacional } from 'src/schemas/languagesWrapperSchema';
 import { HttpStatusCode } from 'src/support/helpers/HttpStatusCode';
 import ValidateEntry from 'src/support/helpers/ValidateEntry';
 import { LoggerType } from 'src/types/LoggerType';
+import UpdateResponse from 'src/types/UpdateResponse';
 
 export default class ArmorsServices extends ValidateEntry implements Service<Internacional<Armor>> {
     constructor(
@@ -15,7 +16,14 @@ export default class ArmorsServices extends ValidateEntry implements Service<Int
     }
 
     public async findAll(): Promise<Array<Internacional<Armor>>> {
-        const response = await this._model.findAll();
+        const response = await this._model.findAll({ active: true });
+
+        this._logger('info', 'All armor entities found with success');
+        return response;
+    }
+
+    public async findAllDisabled(): Promise<Array<Internacional<Armor>>> {
+        const response = await this._model.findAll({ active: false });
 
         this._logger('info', 'All armor entities found with success');
         return response;
@@ -25,7 +33,7 @@ export default class ArmorsServices extends ValidateEntry implements Service<Int
         const response = await this._model.findOne(_id);
 
         if (!response) {
-            const err = new Error('NotFound an armor with provided ID');
+            const err = new Error('NotFound a armor with provided ID');
             err.stack = HttpStatusCode.NOT_FOUND.toString();
             err.name = 'NotFound';
 
@@ -38,12 +46,21 @@ export default class ArmorsServices extends ValidateEntry implements Service<Int
     }
 
     public async update(_id: string, payload: Internacional<Armor>): Promise<Internacional<Armor>> {
-        this.validate(languagesWrapper(armorZodSchema), payload);
+        this.validate(languagesWrapper(armorsZodSchema), payload);
+
+        if (payload.active) {
+            const err = new Error('Not authorized to change availability');
+            err.stack = HttpStatusCode.BAD_REQUEST.toString();
+            err.name = 'BadRequest';
+
+            this._logger('error', err.message);
+            throw err;
+        }
 
         const response = await this._model.update(_id, payload);
 
         if (!response) {
-            const err = new Error('NotFound an armor with provided ID');
+            const err = new Error('NotFound a armor with provided ID');
             err.stack = HttpStatusCode.NOT_FOUND.toString();
             err.name = 'NotFound';
 
@@ -55,17 +72,36 @@ export default class ArmorsServices extends ValidateEntry implements Service<Int
         return response;
     }
 
-    public async delete(_id: string): Promise<void> {
+    public async updateAvailability(_id: string, query: boolean): Promise<UpdateResponse> {
         const response = await this._model.findOne(_id);
 
         if (!response) {
-            const err = new Error('NotFound an armor with provided ID');
+            const err = new Error('NotFound a armor with provided ID');
             err.stack = HttpStatusCode.NOT_FOUND.toString();
             err.name = 'NotFound';
 
+            this._logger('error', err.message);
             throw err;
         }
 
-        await this._model.delete(_id);
+        if (response.active === query) {
+            const err = new Error(`${query ? 'Entity already enabled' : 'Entity already disabled'}`);
+            err.stack = HttpStatusCode.BAD_REQUEST.toString();
+            err.name = 'BadRequest';
+
+            this._logger('error', err.message);
+            throw err;
+        }
+
+        response.active = query;
+        await this._model.update(_id, response);
+
+        const responseMessage = {
+            message: `Armor ${response._id as string} was ${query ? 'activated' : 'deactivated'}`,
+            name: 'success',
+        };
+
+        this._logger('info', 'Armor entity availability updated with success');
+        return responseMessage;
     }
 }
