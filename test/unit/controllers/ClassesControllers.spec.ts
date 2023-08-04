06e6d513014c86bdd98df0ee3c4aeb7d144533f8
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-08-04 00:50:11 -0300

    tests: Add new test for disabled operation and change coverage script

--- test/unit/controllers/ClassesControllers.spec.ts
+++ test/unit/controllers/ClassesControllers.spec.ts
@@ -35,6 +35,26 @@ describe('Services :: ClassesControllers', () => {
         });
     });
 
+    describe('When a request is made to recover all classes disabled', () => {
+        classMockInstance.active = false;
+        beforeAll(() => {
+            response.status = jest.fn().mockReturnValue(response);
+            response.json = jest.fn().mockReturnValue({});
+
+            jest.spyOn(ClassesServicesMock, 'findAllDisabled').mockResolvedValue([classMockInstance]);
+        });
+
+        afterAll(() => {
+            jest.clearAllMocks();
+        });
+
+        it('should return correct data in response json with status 200', async () => {
+            await ClassesControllersMock.findAllDisabled(request, response);
+            expect(response.status).toHaveBeenCalledWith(200);
+            expect(response.json).toHaveBeenCalledWith([classMockInstance]);
+        });
+    });
+
     describe('When a request is made to recover class by ID', () => {
         beforeAll(() => {
             response.status = jest.fn().mockReturnValue(response);
