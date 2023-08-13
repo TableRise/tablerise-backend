d3436497c45b51fef1c371f995a8e92859be18ff
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-08-13 20:47:49 -0300

    fix: Prettier

--- test/unit/support/helpers/generateMongoURI.spec.ts
+++ test/unit/support/helpers/generateMongoURI.spec.ts
@@ -4,7 +4,7 @@ describe('Support :: Helpers :: GenerateMongoURI', () => {
     describe('When called', () => {
         it('should return a valid mongo URI', () => {
             const URI = generateMongoURI('');
-            expect(URI).toBe('mongodb://root:secret@127.0.0.1:27018/dungeons&dragons5e?authSource=admin')
+            expect(URI).toBe('mongodb://root:secret@127.0.0.1:27018/dungeons&dragons5e?authSource=admin');
         });
     });
 });
