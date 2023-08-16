36ff280d268e78efab8ea518763372f40aef4b53
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-08-16 12:25:31 -0300

    ref: Delete unused tests

--- test/connectDatabaseTest.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-import Connections from 'src/database/DatabaseConnection';
-
-Connections['dungeons&dragons5e']
-    .close()
-    .then(() => {})
-    .catch((error) => {
-        throw error;
-    });
