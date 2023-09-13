dfd10d4db5f6825f10674afca7515fac84a7c07e
Cesar Holanda <rasecmh0@gmail.com>
2023-09-13 00:10:23 -0300

    feat: create facebook registration route

--- src/routes/user/swagger.ts
+++ src/routes/user/swagger.ts
@@ -1,3 +1,6 @@
 import { routeOriginal } from '@tablerise/auto-swagger';
 
-export default [['/auth/google', 'OAuth', 'get', null, null, false]] as routeOriginal;
+export default [
+    ['/auth/google', 'OAuth', 'get', null, null, false],
+    ['/auth/facebook', 'OAuth', 'get', null, null, false],
+] as routeOriginal;
