95ce14cd2b52dbf4a3a42ad7dbe28f09702de3c2
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-09-14 17:48:21 -0300

    feat: Add `hashPassword` method for password hashing

--- src/support/helpers/SecurePasswordHandler.ts
+++ src/support/helpers/SecurePasswordHandler.ts
@@ -1,1 +1,15 @@
-export class SecurePasswordHandler {}
+import * as bcrypt from 'bcrypt';
+
+export class SecurePasswordHandler {
+  private static readonly saltRounds = 10;
+
+  static async hashPassword(password: string): Promise<string> {
+    try {
+      const salt = await bcrypt.genSalt(this.saltRounds);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      return hashedPassword;
+    } catch (error) {
+      throw new Error('Error hashing password');
+    }
+  }
+}
