aae3d9389d80d7ff0c033462e6df0703a665272d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-09-14 17:55:32 -0300

    feat: Add `comparePassword` method for password verification

--- src/support/helpers/SecurePasswordHandler.ts
+++ src/support/helpers/SecurePasswordHandler.ts
@@ -1,15 +1,24 @@
 import * as bcrypt from 'bcrypt';
 
 export class SecurePasswordHandler {
-  private static readonly saltRounds = 10;
+    private static readonly saltRounds = 10;
 
-  static async hashPassword(password: string): Promise<string> {
-    try {
-      const salt = await bcrypt.genSalt(this.saltRounds);
-      const hashedPassword = await bcrypt.hash(password, salt);
-      return hashedPassword;
-    } catch (error) {
-      throw new Error('Error hashing password');
+    static async hashPassword(password: string): Promise<string> {
+        try {
+            const salt = await bcrypt.genSalt(this.saltRounds);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            return hashedPassword;
+        } catch (error) {
+            throw new Error('Error hashing password');
+        }
     }
-  }
+
+    static async comparePassword(password: string, hashedPassword: string): Promise<boolean> {
+        try {
+            const match = await bcrypt.compare(password, hashedPassword);
+            return match;
+        } catch (error) {
+            throw new Error('Error comparing password');
+        }
+    }
 }
