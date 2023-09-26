547418abc67ba74a5fd8b8abc847f66608cd81ac
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-09-26 18:55:34 -0300

    fix: remove catchs

--- src/support/helpers/SecurePasswordHandler.ts
+++ src/support/helpers/SecurePasswordHandler.ts
@@ -4,21 +4,13 @@ export class SecurePasswordHandler {
     private static readonly saltRounds = 10;
 
     static async hashPassword(password: string): Promise<string> {
-        try {
-            const salt = await bcrypt.genSalt(this.saltRounds);
-            const hashedPassword = await bcrypt.hash(password, salt);
-            return hashedPassword;
-        } catch (error) {
-            throw new Error('Error hashing password');
-        }
+        const salt = await bcrypt.genSalt(this.saltRounds);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        return hashedPassword;
     }
 
     static async comparePassword(password: string, hashedPassword: string): Promise<boolean> {
-        try {
-            const match = await bcrypt.compare(password, hashedPassword);
-            return match;
-        } catch (error) {
-            throw new Error('Error comparing password');
-        }
+        const match = await bcrypt.compare(password, hashedPassword);
+        return match;
     }
 }
