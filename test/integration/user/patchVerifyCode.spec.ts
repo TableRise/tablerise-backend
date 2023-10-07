7db8d55c072f1b90f27eef375afd740a380bffbb
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-10-07 09:38:05 -0300

    fix: Integration test

--- test/integration/user/patchVerifyCode.spec.ts
+++ test/integration/user/patchVerifyCode.spec.ts
@@ -3,21 +3,12 @@ import logger from '@tablerise/dynamic-logger';
 import requester from '../../support/requester';
 import mock from 'src/support/mocks/user';
 import { HttpStatusCode } from 'src/services/helpers/HttpStatusCode';
+import generateNewMongoID from 'src/support/helpers/generateNewMongoID';
+import EmailSender from 'src/services/user/helpers/EmailSender';
 
-describe('Post user in database', () => {
+describe('Add verify code in database', () => {
     const userInstanceMock = mock.user.user;
-    const userDetailsInstanceMock = mock.user.userDetails;
-
     userInstanceMock.email = `${Math.random()}${userInstanceMock.email}`;
-
-    const { providerId: _, createdAt: _1, updatedAt: _2, tag: _4, ...userInstanceMockPayload } = userInstanceMock;
-    const { userId: _5, ...userDetailsInstanceMockPayload } = userDetailsInstanceMock;
-
-    const userPayload = {
-        ...userInstanceMockPayload,
-        twoFactorSecret: { active: true },
-        details: userDetailsInstanceMockPayload,
-    };
 
     beforeAll(async () => {
         DatabaseManagement.connect(true)
@@ -33,14 +24,17 @@ describe('Post user in database', () => {
         await mongoose.connection.close();
     });
 
-    describe('When validate a confirmation code', () => {
-        it('should return correct data and status', async () => {
-            const userResponse = await requester
-                .post('/profile/register')
-                .send(userPayload)
-                .expect(HttpStatusCode.CREATED);
+    describe('When send a verification code', () => {
+        beforeAll(() => {
+            jest.spyOn(EmailSender.prototype, 'send').mockResolvedValue({ success: true, verificationCode: '' });
+        });
 
-            await requester.get(`/profile/${userResponse.body._id as string}/email-verify`).expect(HttpStatusCode.OK);
+        it('should return correct status', async () => {
+            // @ts-expect-error In progress will exist below
+            userInstanceMock.inProgress.status = 'done';
+            userInstanceMock._id = generateNewMongoID();
+            const userTest = await new DatabaseManagement().modelInstance('user', 'Users').create(userInstanceMock);
+            await requester.get(`/profile/${userTest._id as string}/email-verify`).expect(HttpStatusCode.OK);
         });
     });
 });
