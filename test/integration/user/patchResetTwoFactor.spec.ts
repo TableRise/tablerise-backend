43e2b7ff82f0b3ecc1b215135b3013937654fdb7
steteler <joaovictorkikuti@gmail.com>
2023-10-14 15:02:50 -0300

    feat: implements integration test of resetTwoFactor

---
+++ test/integration/user/patchResetTwoFactor.spec.ts
@@ -0,0 +1,69 @@
+import DatabaseManagement, { mongoose } from '@tablerise/database-management';
+import logger from '@tablerise/dynamic-logger';
+import requester from '../../support/requester';
+import mock from 'src/support/mocks/user';
+import { HttpStatusCode } from 'src/services/helpers/HttpStatusCode';
+import EmailSender from 'src/services/user/helpers/EmailSender';
+import getToken from '../../support/getToken';
+
+describe('Post user in database', () => {
+    const userInstanceMock = mock.user.user;
+    const userDetailsInstanceMock = mock.user.userDetails;
+
+    userInstanceMock.email = `${Math.random()}${userInstanceMock.email}`;
+
+    const { providerId: _, createdAt: _1, updatedAt: _2, tag: _4, ...userInstanceMockPayload } = userInstanceMock;
+    const { userId: _5, ...userDetailsInstanceMockPayload } = userDetailsInstanceMock;
+
+    const userPayload = {
+        ...userInstanceMockPayload,
+        twoFactorSecret: { active: true },
+        details: userDetailsInstanceMockPayload,
+    };
+
+    beforeAll(async () => {
+        DatabaseManagement.connect(true)
+            .then(() => {
+                logger('info', 'Test database connection instanciated');
+            })
+            .catch(() => {
+                logger('error', 'Test database connection failed');
+            });
+    });
+
+    afterAll(async () => {
+        await mongoose.connection.close();
+    });
+
+    describe('When 2FA is reset', () => {
+        beforeAll(() => {
+            jest.spyOn(EmailSender.prototype, 'send').mockResolvedValue({ success: true, verificationCode: 'XRFS78' });
+        });
+
+        it('should return correct new QRCode and Active', async () => {
+            const userResponse = await requester
+                .post('/profile/register')
+                .send(userPayload)
+                .expect(HttpStatusCode.CREATED);
+
+            const userToken = await getToken(userPayload);
+            const userId: string = userResponse.body._id;
+
+            const code: string = userResponse.body.inProgress.code;
+            console.log(userId);
+            console.log(code);
+            console.log(`/profile/${userId}/2fa/reset?code=${code}`);
+
+            const response = await requester
+                .patch(`/profile/${userId}/2fa/reset?code=${code}`)
+                .set('Authorization', `Bearer ${userToken}`)
+                .expect(HttpStatusCode.OK);
+
+            console.log(response);
+
+            expect(response.body).toHaveProperty('qrcode');
+            expect(response.body).toHaveProperty('active');
+            expect(response.body.active).toBe(true);
+        });
+    });
+});
