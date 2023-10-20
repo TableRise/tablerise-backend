de4dcff772232a080933770accd557d467deaecc
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-10-19 22:51:25 -0300

    ref: Remove unnecessary types

--- src/types/contracts/users/controllers/UsersController.d.ts
+++
@@ -1,5 +0,0 @@
-import { HttpStatusCode } from 'src/services/helpers/HttpStatusCode';
-
-export abstract class UsersControllerContract {
-    httpStatusCode: typeof HttpStatusCode;
-}
--- src/types/contracts/users/controllers/UsersRoutes.d.ts
+++
@@ -1,5 +0,0 @@
-import UsersController from 'src/interface/users/presentation/UsersController';
-
-export abstract class UsersRoutesContract {
-    usersController: UsersController
-}
