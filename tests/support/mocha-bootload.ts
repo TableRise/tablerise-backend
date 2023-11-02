66ec0c810ea0258ef6520bf28a13479aaf8181e3
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-11-01 23:11:38 -0300

    test: Implement test with chai, mocha and sinon

---
+++ tests/support/mocha-bootload.ts
@@ -0,0 +1,4 @@
+import chai from 'chai';
+chai.use(require('dirty-chai'));
+// @ts-expect-error Will create a new global property
+global.expect = chai.expect;
---
+++ tests/unit/interface/users/presentation/users/UsersRoutes.spec.ts
@@ -0,0 +1,31 @@
+import AuthorizationMiddleware from 'src/interface/users/middlewares/AuthorizationMiddleware';
+import VerifyEmailCodeMiddleware from 'src/interface/users/middlewares/VerifyEmailCodeMiddleware';
+import UsersController from 'src/interface/users/presentation/users/UsersController';
+import UsersRoutes from 'src/interface/users/presentation/users/UsersRoutes';
+
+describe('Interface :: Users :: Presentation :: Users :: UsersRoutes', () => {
+    let usersRoutes: UsersRoutes,
+    usersController,
+    verifyIdMiddleware,
+    authorizationMiddleware,
+    verifyEmailCodeMiddleware;
+
+    context('When all the routes are correctly implemented', () => {
+        usersController = {} as UsersController;
+        verifyIdMiddleware = () => ({});
+        authorizationMiddleware = {} as AuthorizationMiddleware;
+        verifyEmailCodeMiddleware = {} as VerifyEmailCodeMiddleware;
+
+        usersRoutes = new UsersRoutes({
+            usersController,
+            authorizationMiddleware,
+            verifyIdMiddleware,
+            verifyEmailCodeMiddleware,
+        });
+
+        it('Should return the correct number of routes', () => {
+            const routes = usersRoutes.routes();
+            expect(routes).to.have.lengthOf(13);
+        });
+    });
+});
