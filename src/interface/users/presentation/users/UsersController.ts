6a0dd5be012003e80cf7fa5b6ccca102d1fa27ec
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-11-04 19:18:15 -0300

    ref: Inject operations in controller

--- src/interface/users/presentation/users/UsersController.ts
+++ src/interface/users/presentation/users/UsersController.ts
@@ -1,56 +1,94 @@
 import { Response, Request } from 'express';
-import { container } from 'src/container';
-import { RegisterUserPayload } from 'src/types/users/requests/Payload';
+import { RegisterUserPayload, UpdateGameInfoPayload, VerifyEmailPayload } from 'src/types/users/requests/Payload';
 import { HttpStatusCode } from 'src/infra/helpers/common/HttpStatusCode';
 import { UsersControllerContract } from 'src/types/users/contracts/presentation/UsersController';
 
 export default class UsersController {
     private readonly _createUserOperation;
+    private readonly _updateUserOperation;
+    private readonly _verifyEmailOperation;
+    private readonly _getUsersOperation;
+    private readonly _getUserByIdOperation;
+    private readonly _confirmCodeOperation;
+    private readonly _activateTwoFactorOperation;
+    private readonly _resetTwoFactorOperation;
+    private readonly _updateEmailOperation;
+    private readonly _updateGameInfoOperation;
+    private readonly _resetProfileOperation;
+    private readonly _deleteUserOperation;
 
-    constructor({ createUserOperation }: UsersControllerContract) {
+    constructor({
+        createUserOperation,
+        updateUserOperation,
+        verifyEmailOperation,
+        getUsersOperation,
+        getUserByIdOperation,
+        confirmCodeOperation,
+        activateTwoFactorOperation,
+        resetTwoFactorOperation,
+        updateEmailOperation,
+        updateGameInfoOperation,
+        resetProfileOperation,
+        deleteUserOperation
+    }: UsersControllerContract) {
         this._createUserOperation = createUserOperation;
+        this._updateUserOperation = updateUserOperation;
+        this._verifyEmailOperation = verifyEmailOperation;
+        this._getUsersOperation = getUsersOperation;
+        this._getUserByIdOperation = getUserByIdOperation;
+        this._confirmCodeOperation = confirmCodeOperation;
+        this._activateTwoFactorOperation = activateTwoFactorOperation;
+        this._resetTwoFactorOperation = resetTwoFactorOperation;
+        this._updateEmailOperation = updateEmailOperation;
+        this._updateGameInfoOperation = updateGameInfoOperation;
+        this._resetProfileOperation = resetProfileOperation;
+        this._deleteUserOperation = deleteUserOperation;
 
         this.register = this.register.bind(this);
+        this.update = this.update.bind(this);
+        this.verifyEmail = this.verifyEmail.bind(this);
+        this.getUsers = this.getUsers.bind(this);
+        this.getUserById = this.getUserById.bind(this);
+        this.confirmCode = this.confirmCode.bind(this);
+        this.activateTwoFactor = this.activateTwoFactor.bind(this);
+        this.resetTwoFactor = this.resetTwoFactor.bind(this);
+        this.updateEmail = this.updateEmail.bind(this);
+        this.updateGameInfo = this.updateGameInfo.bind(this);
+        this.resetProfile = this.resetTwoFactor.bind(this);
+        this.delete = this.delete.bind(this);
     }
 
     public async register(req: Request, res: Response): Promise<Response> {
         const payload = req.body as RegisterUserPayload;
-
+ 
         const result = await this._createUserOperation.execute(payload);
         return res.status(HttpStatusCode.CREATED).json(result);
     }
 
     public async update(req: Request, res: Response): Promise<Response> {
-        const { execute } = container.resolve('updateUserOperation');
-
         const { id } = req.params;
         const payload = req.body as RegisterUserPayload;
 
-        const result = await execute({ userId: id, payload });
+        const result = await this._updateUserOperation.execute({ userId: id, payload });
         return res.status(HttpStatusCode.CREATED).json(result);
     }
 
     public async verifyEmail(req: Request, res: Response): Promise<Response> {
-        const { execute } = container.resolve('verifyEmailOperation');
+        const { email, newEmail } = req.query as unknown as VerifyEmailPayload;
 
-        const { email, newEmail } = req.query;
-
-        const result = await execute({ email, newEmail });
-        return res.status(HttpStatusCode.NO_CONTENT).json(result);
+        await this._verifyEmailOperation.execute({ email, newEmail });
+        return res.status(HttpStatusCode.NO_CONTENT).end();
     }
 
     public async getUsers(req: Request, res: Response): Promise<Response> {
-        const { execute } = container.resolve('getUsersOperation');
-        const result = await execute();
+        const result = await this._getUsersOperation.execute();
         return res.status(HttpStatusCode.OK).json(result);
     }
 
     public async getUserById(req: Request, res: Response): Promise<Response> {
-        const { execute } = container.resolve('getUserByIdOperation');
-
         const { id } = req.params;
 
-        const result = await execute({ userId: id });
+        const result = await this._getUserByIdOperation.execute({ userId: id });
         return res.status(HttpStatusCode.OK).json(result);
     }
 
@@ -60,70 +98,57 @@ export default class UsersController {
     }
 
     public async confirmCode(req: Request, res: Response): Promise<Response> {
-        const { execute } = container.resolve('confirmCodeOperation');
 
         const { id } = req.params;
-        const { code } = req.query;
+        const { code } = req.query as { code: string };
 
-        const result = await execute({ userId: id, code });
+        const result = await this._confirmCodeOperation.execute({ userId: id, code });
         return res.status(HttpStatusCode.OK).json(result);
     }
 
     public async activateTwoFactor(req: Request, res: Response): Promise<Response> {
-        const { execute } = container.resolve('activateTwoFactorOperation');
-
         const { id } = req.params;
 
-        const result = await execute(id);
+        const result = await this._activateTwoFactorOperation.execute(id);
         return res.status(HttpStatusCode.OK).json(result);
     }
 
     public async resetTwoFactor(req: Request, res: Response): Promise<Response> {
-        const { execute } = container.resolve('resetTwoFactorOperation');
-
         const { id } = req.params;
-        const { code } = req.query;
+        const { code } = req.query as { code: string };
 
-        const result = await execute({ userId: id, code });
+        const result = await this._resetTwoFactorOperation.execute({ userId: id, code });
         return res.status(HttpStatusCode.OK).json(result);
     }
 
     public async updateEmail(req: Request, res: Response): Promise<Response> {
-        const { execute } = container.resolve('updateEmailOperation');
-
         const { id } = req.params;
-        const { code } = req.query;
+        const { code } = req.query as { code: string };;
         const { email } = req.body;
 
-        const result = await execute({ userId: id, code, email });
-        return res.status(HttpStatusCode.OK).json(result);
+        await this._updateEmailOperation.execute({ userId: id, code, email });
+        return res.status(HttpStatusCode.OK).end();
     }
 
     public async updateGameInfo(req: Request, res: Response): Promise<Response> {
-        const { execute } = container.resolve('updateGameInfoOperation');
-
         const { id } = req.params;
-        const { infoId, targetInfo, operation } = req.query;
+        const { infoId, targetInfo, operation } = req.query as unknown as UpdateGameInfoPayload;
 
-        const result = await execute({ userId: id, infoId, targetInfo, operation });
+        const result = await this._updateGameInfoOperation.execute({ userId: id, infoId, targetInfo, operation });
         return res.status(HttpStatusCode.OK).json(result);
     }
 
     public async resetProfile(req: Request, res: Response): Promise<Response> {
-        const { execute } = container.resolve('resetProfileOperation');
-
         const { id } = req.params;
 
-        await execute(id);
+        await this._resetProfileOperation.execute(id);
         return res.status(HttpStatusCode.NO_CONTENT).end();
     }
 
     public async delete(req: Request, res: Response): Promise<Response> {
-        const { execute } = container.resolve('deleteUserOperation');
-
         const { id } = req.params;
 
-        await execute(id);
+        await this._deleteUserOperation.execute(id);
         return res.status(HttpStatusCode.NO_CONTENT).end();
     }
 }
--- src/types/users/contracts/presentation/UsersController.ts
+++ src/types/users/contracts/presentation/UsersController.ts
@@ -1,5 +1,27 @@
+import ActivateTwoFactorOperation from 'src/core/users/operations/users/ActivateTwoFactorOperation';
+import ConfirmCodeOperation from 'src/core/users/operations/users/ConfirmCodeOperation';
 import CreateUserOperation from 'src/core/users/operations/users/CreateUserOperation';
+import DeleteUserOperation from 'src/core/users/operations/users/DeleteUserOperation';
+import GetUserByIdOperation from 'src/core/users/operations/users/GetUserByIdOperation';
+import GetUsersOperation from 'src/core/users/operations/users/GetUsersOperation';
+import ResetProfileOperation from 'src/core/users/operations/users/ResetProfileOperation';
+import ResetTwoFactorOperation from 'src/core/users/operations/users/ResetTwoFactorOperation';
+import UpdateEmailOperation from 'src/core/users/operations/users/UpdateEmailOperation';
+import UpdateGameInfoOperation from 'src/core/users/operations/users/UpdateGameInfoOperation';
+import UpdateUserOperation from 'src/core/users/operations/users/UpdateUserOperation';
+import VerifyEmailOperation from 'src/core/users/operations/users/VerifyEmailOperation';
 
 export interface UsersControllerContract {
     createUserOperation: CreateUserOperation;
+    updateUserOperation: UpdateUserOperation;
+    verifyEmailOperation: VerifyEmailOperation;
+    getUsersOperation: GetUsersOperation;
+    getUserByIdOperation: GetUserByIdOperation;
+    confirmCodeOperation: ConfirmCodeOperation;
+    activateTwoFactorOperation: ActivateTwoFactorOperation;
+    resetTwoFactorOperation: ResetTwoFactorOperation;
+    updateEmailOperation: UpdateEmailOperation;
+    updateGameInfoOperation: UpdateGameInfoOperation;
+    resetProfileOperation: ResetProfileOperation;
+    deleteUserOperation: DeleteUserOperation;
 }
