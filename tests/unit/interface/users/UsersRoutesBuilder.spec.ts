79b606dacb184dc0478bfeee7e2cc201bb712943
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-11-16 10:45:30 -0300

    test: Create tests for UsersRoutesBuilder

---
+++ tests/unit/interface/users/UsersRoutesBuilder.spec.ts
@@ -0,0 +1,63 @@
+import { Router } from 'express';
+import UsersRoutesBuilder from 'src/interface/users/UsersRoutesBuilder';
+
+describe('Interface :: Users :: UsersRoutesBuilder', () => {
+    let usersRoutesBuilder: UsersRoutesBuilder,
+    usersRoutes: any,
+    oAuthRoutes: any,
+    verifyIdMiddleware: any
+
+    context('When profile routes are processed', () => {
+        verifyIdMiddleware = () => ({});
+
+        usersRoutes = {
+            routes: () => [{
+                method: 'get',
+                path: '/base/api',
+                options: {
+                    middlewares: [verifyIdMiddleware],
+                    authentication: false
+                }
+            }]
+        };
+
+        beforeEach(() => {
+            oAuthRoutes = { routes: () => [] };
+            usersRoutesBuilder = new UsersRoutesBuilder({ usersRoutes, oAuthRoutes });
+        });
+
+        it('should return correct properties', () => {
+            const routes = usersRoutesBuilder.get();
+
+            expect(routes.usersRoutes).to.have.property('profile');
+            expect(typeof routes.usersRoutes.profile).to.be.equal(typeof Router);
+        });
+    });
+
+    context('When oauth routes are processed', () => {
+        verifyIdMiddleware = () => ({});
+
+        oAuthRoutes = {
+            routes: () => [{
+                method: 'get',
+                path: '/base/api',
+                options: {
+                    middlewares: [verifyIdMiddleware],
+                    authentication: false
+                }
+            }]
+        };
+
+        beforeEach(() => {
+            usersRoutes = { routes: () => [] };
+            usersRoutesBuilder = new UsersRoutesBuilder({ usersRoutes, oAuthRoutes });
+        });
+
+        it('should return correct properties', () => {
+            const routes = usersRoutesBuilder.get();
+
+            expect(routes.usersRoutes).to.have.property('oAuth');
+            expect(typeof routes.usersRoutes.oAuth).to.be.equal(typeof Router);
+        });
+    });
+});
