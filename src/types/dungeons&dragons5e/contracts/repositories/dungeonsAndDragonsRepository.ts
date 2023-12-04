17090c18716b1fd0895ec61d22de5d6b9170cd07
Cesar Holanda <rasecmh0@gmail.com>
2023-12-04 15:11:48 -0300

    test: create unit test for d&d repository

--- src/types/dungeons&dragons5e/contracts/repositories/dungeonsAndDragonsRepository.ts
+++ src/types/dungeons&dragons5e/contracts/repositories/dungeonsAndDragonsRepository.ts
@@ -1,8 +1,7 @@
-import DatabaseManagement, { MongoModel } from '@tablerise/database-management';
+import DatabaseManagement from '@tablerise/database-management';
 import { Logger } from 'src/types/Logger';
 
 export interface DungeonsAndDragonsRepositoryContract {
     database: DatabaseManagement;
-    model: MongoModel<unknown>;
     logger: Logger;
 }
---
+++ tests/unit/infra/repositories/dungeons&dragons5e/DungeonsAndDragonsRepository.spec.ts
@@ -0,0 +1,176 @@
+import sinon from 'sinon';
+import HttpRequestErrors from 'src/domains/common/helpers/HttpRequestErrors';
+import { HttpStatusCode } from 'src/domains/common/helpers/HttpStatusCode';
+import DungeonsAndDragonsRepository from 'src/infra/repositories/dungeons&dragons5e/DungeonsAndDragonsRepository';
+import { Logger } from 'src/types/Logger';
+
+describe('Infra :: Repositories :: DungeonsAndDragon :: DungeonsAndDragonsRepository', () => {
+    let dungeonsAndDragonsRepository: DungeonsAndDragonsRepository, database: any;
+
+    const logger: Logger = () => {};
+
+    context('#find', () => {
+        const findAll = sinon.spy(() => [{ name: 'Couro' }]);
+
+        beforeEach(() => {
+            database = {
+                modelInstance: () => ({
+                    findAll,
+                }),
+            };
+
+            dungeonsAndDragonsRepository = new DungeonsAndDragonsRepository({
+                database,
+                logger,
+            });
+
+            dungeonsAndDragonsRepository.setEntity('Items');
+        });
+
+        it('should find dungeonsAndDragons entity and return serialized', async () => {
+            const result: any = await dungeonsAndDragonsRepository.find({});
+
+            expect(findAll).to.have.been.called();
+            expect(result).to.be.an('array').that.has.length(1);
+            expect(result[0]).to.have.property('name');
+            expect(result[0].name).to.be.equal('Couro');
+        });
+
+        it('should find dungeonsAndDragons entity and return serialized - query undefined', async () => {
+            const result: any = await dungeonsAndDragonsRepository.find();
+
+            expect(findAll).to.have.been.called();
+            expect(result).to.be.an('array').that.has.length(1);
+            expect(result[0]).to.have.property('name');
+            expect(result[0].name).to.be.equal('Couro');
+        });
+    });
+
+    context('#findOne', () => {
+        context('when is found', () => {
+            const findOne = sinon.spy(() => ({ name: 'Couro' }));
+
+            beforeEach(() => {
+                database = {
+                    modelInstance: sinon.spy(() => ({
+                        findOne,
+                    })),
+                };
+
+                dungeonsAndDragonsRepository = new DungeonsAndDragonsRepository({
+                    database,
+                    logger,
+                });
+            });
+
+            it('should find an dungeonsAndDragons entity and return serialized', async () => {
+                const result: any = await dungeonsAndDragonsRepository.findOne({
+                    name: 'Couro',
+                });
+
+                expect(findOne).to.have.been.called();
+                expect(result).to.have.property('name');
+                expect(result.name).to.be.equal('Couro');
+            });
+
+            it('should find an dungeonsAndDragons entity and return serialized - query undefined', async () => {
+                const result: any = await dungeonsAndDragonsRepository.findOne();
+
+                expect(findOne).to.have.been.called();
+                expect(result).to.have.property('name');
+                expect(result.name).to.be.equal('Couro');
+            });
+        });
+
+        context('when is not found', () => {
+            beforeEach(() => {
+                database = {
+                    modelInstance: () => ({
+                        findOne: () => null,
+                    }),
+                };
+
+                dungeonsAndDragonsRepository = new DungeonsAndDragonsRepository({
+                    database,
+                    logger,
+                });
+            });
+
+            it('should find an dungeonsAndDragons and return serialized', async () => {
+                try {
+                    await dungeonsAndDragonsRepository.findOne({
+                        name: 'Couro',
+                    });
+                } catch (error) {
+                    const err = error as HttpRequestErrors;
+                    expect(err.message).to.be.equal(
+                        'This content do not exist in the RPG system'
+                    );
+                    expect(err.code).to.be.equal(HttpStatusCode.NOT_FOUND);
+                    expect(err.name).to.be.equal('NotFound');
+                }
+            });
+        });
+    });
+
+    context('#update', () => {
+        context('when is found', () => {
+            const update = sinon.spy(() => ({ name: 'Couro' }));
+
+            beforeEach(() => {
+                database = {
+                    modelInstance: () => ({
+                        update,
+                    }),
+                };
+
+                dungeonsAndDragonsRepository = new DungeonsAndDragonsRepository({
+                    database,
+                    logger,
+                });
+            });
+
+            it('should update an dungeonsAndDragons entity and return serialized', async () => {
+                const result: any = await dungeonsAndDragonsRepository.update({
+                    query: { name: 'Pote' },
+                    payload: { name: 'Couro' },
+                });
+
+                expect(update).to.have.been.called();
+                expect(result).to.have.property('name');
+                expect(result.name).to.be.equal('Couro');
+            });
+        });
+
+        context('when is not found', () => {
+            beforeEach(() => {
+                database = {
+                    modelInstance: sinon.spy(() => ({
+                        update: () => null,
+                    })),
+                };
+
+                dungeonsAndDragonsRepository = new DungeonsAndDragonsRepository({
+                    database,
+                    logger,
+                });
+            });
+
+            it('should update an dungeonsAndDragons entity and return serialized', async () => {
+                try {
+                    await dungeonsAndDragonsRepository.update({
+                        query: { name: 'Pote' },
+                        payload: { name: 'Couro' },
+                    });
+                } catch (error) {
+                    const err = error as HttpRequestErrors;
+                    expect(err.message).to.be.equal(
+                        'This content do not exist in the RPG system'
+                    );
+                    expect(err.code).to.be.equal(HttpStatusCode.NOT_FOUND);
+                    expect(err.name).to.be.equal('NotFound');
+                }
+            });
+        });
+    });
+});
