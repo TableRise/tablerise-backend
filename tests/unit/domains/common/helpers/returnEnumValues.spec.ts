575476b42ed3e11e8ad309df970b62480958d8ee
Cesar Holanda <rasecmh0@gmail.com>
2023-12-11 15:14:46 -0300

    test: ref descriptions in returnEnumValues unit tests

--- tests/unit/domains/common/helpers/returnEnumValues.spec.ts
+++ tests/unit/domains/common/helpers/returnEnumValues.spec.ts
@@ -5,11 +5,11 @@ describe('Domains :: Common :: Helpers :: ReturnEnumValues', () => {
     const enumValuesFixed = ['he/his', 'she/her', 'they/them', 'he/his - she/her', 'any'];
 
     context('When enum values', () => {
-        it('should have correct key/values', () => {
+        it('should have correct values', () => {
             const enumTest = returnEnumValues(pronounEnum.enum);
 
-            enumValuesFixed.forEach((key: string, index: number) => {
-                expect(enumTest[index]).to.be.equal(key);
+            enumValuesFixed.forEach((value: string, index: number) => {
+                expect(enumTest[index]).to.be.equal(value);
             });
         });
     });
