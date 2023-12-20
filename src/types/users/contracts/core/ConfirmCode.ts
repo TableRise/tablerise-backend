77255920ab7602917e2c6a2e1012eba03ef3e734
steteler <joaovictorkikuti@gmail.com>
2023-12-19 21:37:11 -0300

    refactor: remove httpRequestErros from interface

--- src/types/users/contracts/core/ConfirmCode.ts
+++ src/types/users/contracts/core/ConfirmCode.ts
@@ -1,5 +1,4 @@
 import ConfirmCodeService from 'src/core/users/services/users/ConfirmCodeService';
-import HttpRequestErrors from 'src/domains/common/helpers/HttpRequestErrors';
 import UsersRepository from 'src/infra/repositories/user/UsersRepository';
 import { Logger } from 'src/types/Logger';
 
@@ -9,7 +8,6 @@ export interface ConfirmCodeOperationContract {
 }
 
 export interface ConfirmCodeServiceContract {
-    httpRequestErrors: HttpRequestErrors;
     usersRepository: UsersRepository;
     logger: Logger;
 }
