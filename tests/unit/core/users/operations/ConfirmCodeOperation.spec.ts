2b1dffa57c19407fcc62d7c0fb04c111ffcb4bbc
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-12-20 16:28:06 -0300

    fix: Fix mock restoring of tests

--- tests/unit/core/users/operations/ConfirmCodeOperation.spec.ts
+++ tests/unit/core/users/operations/ConfirmCodeOperation.spec.ts
@@ -5,9 +5,9 @@ describe('Core :: Users :: Operations :: ConfirmCodeOperation', () => {
     let confirmCodeOperation: ConfirmCodeOperation;
     let confirmCodeService: any;
 
-    context('When confirm code with success', () => {
-        const loggerMock = sinon.stub();
+    const logger = (): void => {};
 
+    context('When confirm code with success', () => {
         before(() => {
             confirmCodeService = {
                 processCode: sinon.spy(() => ({ status: 'done' })),
@@ -15,12 +15,8 @@ describe('Core :: Users :: Operations :: ConfirmCodeOperation', () => {
 
             confirmCodeOperation = new ConfirmCodeOperation({
                 confirmCodeService,
-                logger: loggerMock,
+                logger,
             });
-        });
-
-        afterEach(() => {
-            sinon.restore();
         });
 
         it('should return the correct status and call correct methods', async () => {
@@ -29,11 +25,8 @@ describe('Core :: Users :: Operations :: ConfirmCodeOperation', () => {
             const expectedStatus = 'done';
 
             const { status } = await confirmCodeOperation.execute({ userId, code });
-
-            expect(loggerMock).to.be.calledOnce();
             expect(status).to.be.equal(expectedStatus);
-
-            expect(confirmCodeService.processCode).to.have.been.calledOnce();
+            expect(confirmCodeService.processCode).to.have.been.called();
         });
     });
 });
--- tests/unit/core/users/services/ConfirmCodeService.spec.ts
+++ tests/unit/core/users/services/ConfirmCodeService.spec.ts
@@ -4,85 +4,85 @@ import HttpRequestErrors from 'src/domains/common/helpers/HttpRequestErrors';
 import { HttpStatusCode } from 'src/domains/common/helpers/HttpStatusCode';
 
 describe('Core :: Users :: Services :: ConfirmCodeService', () => {
+    let confirmCodeService: ConfirmCodeService;
     let usersRepositoryMock: any;
-    let loggerMock: any;
-    let confirmCodeService: any;
+
+    const logger = (): void => {};
 
     context('When the method to confirm code is called', () => {
-        beforeEach(() => {
+        const userId = 'anyId';
+        const code = 'validCode';
+        const userInDb = {
+            userId,
+            inProgress: {
+                code: 'validCode',
+                status: 'inProgress',
+            },
+        };
+        const updatedUser = {
+            userId,
+            inProgress: {
+                code: 'validCode',
+                status: 'done',
+            },
+        };
+
+        before(() => {
             usersRepositoryMock = {
-                findOne: sinon.stub(),
-                update: sinon.stub(),
+                findOne: sinon.spy(() => userInDb),
+                update: sinon.spy(() => ({})),
             };
-            loggerMock = sinon.stub();
 
             confirmCodeService = new ConfirmCodeService({
                 usersRepository: usersRepositoryMock,
-                logger: loggerMock,
+                logger,
             });
         });
 
-        afterEach(() => {
-            sinon.restore();
-        });
-
         it('should process code correctly', async () => {
-            const userId = 'anyId';
-            const code = 'validCode';
-            const userInDb = {
-                userId,
-                inProgress: {
-                    code: 'validCode',
-                    status: 'inProgress',
-                },
-            };
-
-            usersRepositoryMock.findOne.withArgs({ userId }).resolves(userInDb);
-
             await confirmCodeService.processCode({ userId, code });
 
-            expect(loggerMock).to.be.calledOnce();
-            expect(usersRepositoryMock.findOne).to.be.calledOnce();
-            expect(usersRepositoryMock.update).to.be.calledOnce();
-
-            const updatedUser = {
-                userId,
-                inProgress: {
-                    code: 'validCode',
-                    status: 'done',
-                },
-            };
-
-            expect(usersRepositoryMock.update).to.be.calledWith({
+            expect(usersRepositoryMock.findOne).to.have.been.called();
+            expect(usersRepositoryMock.update).to.have.been.called();
+            expect(usersRepositoryMock.update).to.have.been.calledWith({
                 query: { userId },
                 payload: updatedUser,
             });
         });
+    });
 
-        it('should throw error for invalid code', async () => {
-            const userId = 'anyId';
-            const code = 'invalidCode';
-            const userInDb = {
-                userId,
-                inProgress: {
-                    code: 'validCode',
-                    status: 'inProgress',
-                },
+    context('When the method to confirm code is called - invalid code', () => {
+        const userId = 'anyId';
+        const code = 'invalidCode';
+        const userInDb = {
+            userId,
+            inProgress: {
+                code: 'validCode',
+                status: 'inProgress',
+            },
+        };
+        
+        before(() => {
+            usersRepositoryMock = {
+                findOne: sinon.spy(() => userInDb),
+                update: sinon.spy(() => ({})),
             };
 
-            usersRepositoryMock.findOne.withArgs({ userId }).resolves(userInDb);
+            confirmCodeService = new ConfirmCodeService({
+                usersRepository: usersRepositoryMock,
+                logger,
+            });
+        });
 
+        it('should throw error for invalid code', async () => {
             try {
                 await confirmCodeService.processCode({ userId, code });
-                expect(loggerMock).to.be.calledOnce();
-                expect(usersRepositoryMock.findOne).to.be.calledOnce();
             } catch (error) {
                 const err = error as HttpRequestErrors;
                 expect(err).to.be.instanceOf(HttpRequestErrors);
                 expect(err.message).to.equal('Invalid email verify code');
                 expect(err.code).to.equal(HttpStatusCode.BAD_REQUEST);
                 expect(err.name).to.equal('BadRequest');
-                expect(usersRepositoryMock.update).to.be.not.called();
             }
         });
     });
