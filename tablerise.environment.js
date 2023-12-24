82fd4f7d21dd3e10bc5a12a2564164116fb550a9
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-12-24 00:58:38 -0300

    fix: Some changes

--- tablerise.environment.js
+++ tablerise.environment.js
@@ -5,5 +5,5 @@ module.exports = {
     database_password: process.env.MONGODB_PASSWORD,
     database_host: process.env.MONGODB_HOST,
     database_database: process.env.MONGODB_DATABASE,
-    database_initialString: process.env.MONGODB_CONNECTION_INITIAL,
+    database_initialString: process.env.MONGODB_CONNECTION_INITIAL
 };
