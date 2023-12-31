bf8a0fe1ca78e7700f360895036006cfe8eb3391
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2023-12-30 21:16:16 -0300

    ref: Remove unnecessary code

--- src/domains/common/helpers/swaggerGenerator.ts
+++ src/domains/common/helpers/swaggerGenerator.ts
@@ -12,8 +12,7 @@ export default ({ routesWrapper }: { routesWrapper: RoutesWrapper }): Router => 
 
     if (process.env.NODE_ENV === 'develop') {
         autoSwagger(routesWrapper.declareRoutes()['dungeons&dragons5e'], {
-            title: 'dungeons&dragons5e',
-            url: process.env.SWAGGER_URL ?? 'http://localhost:3001',
+            title: 'dungeons&dragons5e'
         })
             .then((_result: any) => {
                 logger('info', 'Swagger - dungeons&dragons5e - document generated');
@@ -23,8 +22,7 @@ export default ({ routesWrapper }: { routesWrapper: RoutesWrapper }): Router => 
             });
 
         autoSwagger(routesWrapper.declareRoutes().user, {
-            title: 'user',
-            url: process.env.SWAGGER_URL ?? 'http://localhost:3001',
+            title: 'user'
         })
             .then((_result: any) => {
                 logger('info', 'SwaggerGenerator - user - document generated');
