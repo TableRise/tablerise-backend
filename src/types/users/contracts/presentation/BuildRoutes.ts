3424ab7bfa63448e478f6d43025ca71ab8197c76
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2024-01-04 08:06:47 -0300

    ref: Change typing to d.ts - 05

--- src/types/users/contracts/presentation/BuildRoutes.ts
+++
@@ -1,7 +0,0 @@
-import OAuthRoutes from 'src/interface/users/presentation/oauth/OAuthRoutes';
-import UsersRoutes from 'src/interface/users/presentation/users/UsersRoutes';
-
-export interface UsersRoutesBuilderContract {
-    usersRoutes: UsersRoutes;
-    oAuthRoutes: OAuthRoutes;
-}
