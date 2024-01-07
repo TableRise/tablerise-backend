7260d26b2136055eba7a3f4421dfa9ff557952af
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2024-01-07 03:32:45 -0300

    ref: Change coverage configs

--- src/infra/connection/SocketIO.ts
+++ src/infra/connection/SocketIO.ts
@@ -3,16 +3,18 @@ import { Server } from 'http';
 import InfraDependencies from 'src/types/modules/infra/InfraDependencies';
 
 export default class SocketIO {
+    private _socketInstance = {} as socket.Socket;
     private readonly _logger;
 
     constructor({ logger }: InfraDependencies['socketIOContract']) {
         this._logger = logger;
+
+        this.connect = this.connect.bind(this);
+        this._joinTableSocketEvent = this._joinTableSocketEvent.bind(this);
     }
 
     public async connect(httpServer: Server): Promise<void> {
         this._logger('info', 'Connect - SocketIO', true);
-
-        const tables = {};
 
         const io = new socket.Server(httpServer, {
             cors: {
@@ -22,12 +24,16 @@ export default class SocketIO {
         });
 
         io.on('connection', (socket) => {
-            socket.on('join', async (table) => {
-                await socket.join(table);
-                const tableData = tables[table as keyof typeof tables] || { objects: [], images: [] };
-                // @ts-expect-error Will have
-                socket.emit('Joined at TableRise', tableData.objects, tableData.images);
-            });
+            this._socketInstance = socket;
+            socket.on('join', this._joinTableSocketEvent);
         });
+    }
+
+    private async _joinTableSocketEvent(table: string): Promise<void> {
+        const tables = {};
+        await this._socketInstance.join(table);
+        const tableData = tables[table as keyof typeof tables] || { objects: [], images: [] };
+        // @ts-expect-error Will have
+        this._socketInstance.emit('Joined at TableRise', tableData.objects, tableData.images);
     }
 }
