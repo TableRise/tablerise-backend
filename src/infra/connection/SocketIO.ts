442beb0f124c242f6fd2b553b5fc9d10420ddfdd
Cesar Holanda <rasecmh0@gmail.com>
2024-01-18 18:30:48 -0300

    ref: change arrow functions to methods

--- src/infra/connection/SocketIO.ts
+++ src/infra/connection/SocketIO.ts
@@ -1,60 +1,37 @@
 import socket from 'socket.io';
-import { Server } from 'http';
 import InfraDependencies from 'src/types/modules/infra/InfraDependencies';
 
 export default class SocketIO {
     private _socketInstance = {} as socket.Socket;
     private readonly _tables = {} as any;
+    private readonly _io = {} as socket.Server;
     private readonly _logger;
 
-    constructor({ logger }: InfraDependencies['socketIOContract']) {
+    constructor({ logger, httpServer }: InfraDependencies['socketIOContract']) {
         this._logger = logger;
-
-        this.connect = this.connect.bind(this);
-        this._joinTableSocketEvent = this._joinTableSocketEvent.bind(this);
-    }
-
-    public async connect(httpServer: Server): Promise<void> {
-        this._logger('info', 'Connect - SocketIO', true);
-
-        const io = new socket.Server(httpServer, {
+        this._io = new socket.Server(httpServer, {
             cors: {
                 origin: '*',
                 allowedHeaders: ['access_key'],
             },
         });
 
-        io.on('connection', (socket) => {
+        this.connect = this.connect.bind(this);
+        this._joinTableSocketEvent = this._joinTableSocketEvent.bind(this);
+        this._changeBackgroundSocketEvent = this._changeBackgroundSocketEvent.bind(this);
+        this._uploadImageSocketEvent = this._uploadImageSocketEvent.bind(this);
+        this._disconnectSocketEvent = this._disconnectSocketEvent.bind(this);
+    }
+
+    public async connect(): Promise<void> {
+        this._logger('info', 'Connect - SocketIO', true);
+
+        this._io.on('connection', (socket) => {
             this._socketInstance = socket;
             socket.on('join', this._joinTableSocketEvent);
-            socket.on(
-                'changeBackground',
-                async (table: string, newBackground: string): Promise<void> => {
-                    this._tables[table as keyof typeof this._tables].imagens.push(
-                        newBackground
-                    );
-                    io.to(this._tables).emit('backgroundChanged', newBackground);
-                }
-            );
-            socket.on(
-                'uploadImage',
-                async (
-                    table: string,
-                    squareId: string,
-                    imageData: string
-                ): Promise<void> => {
-                    this._tables[table as keyof typeof this._tables].objects.map(
-                        (object: any) =>
-                            object.elementID === parseInt(squareId)
-                                ? { ...object, image: imageData }
-                                : object
-                    );
-                    io.to(this._tables).emit('updateObjectImage', squareId, imageData);
-                }
-            );
-            socket.on('disconnect', async (): Promise<void> => {
-                this._logger('info', 'User disconnected', true);
-            });
+            socket.on('changeBackground', this._changeBackgroundSocketEvent);
+            socket.on('uploadImage', this._uploadImageSocketEvent);
+            socket.on('disconnect', this._disconnectSocketEvent);
         });
     }
     // Remove this comment.
@@ -70,5 +47,30 @@ export default class SocketIO {
             tableData.objects,
             tableData.images
         );
+    }
+
+    private async _changeBackgroundSocketEvent(
+        table: string,
+        newBackground: string
+    ): Promise<void> {
+        this._tables[table as keyof typeof this._tables].imagens.push(newBackground);
+        this._io.to(this._tables).emit('backgroundChanged', newBackground);
+    }
+
+    private async _uploadImageSocketEvent(
+        table: string,
+        squareId: string,
+        imageData: string
+    ): Promise<void> {
+        this._tables[table as keyof typeof this._tables].objects.map((object: any) =>
+            object.elementID === parseInt(squareId)
+                ? { ...object, image: imageData }
+                : object
+        );
+        this._io.to(this._tables).emit('updateObjectImage', squareId, imageData);
+    }
+
+    private async _disconnectSocketEvent(): Promise<void> {
+        this._logger('info', 'User disconnected', true);
     }
 }
--- src/types/modules/infra/connection/SocketIO.d.ts
+++ src/types/modules/infra/connection/SocketIO.d.ts
@@ -1,5 +1,9 @@
 import { Logger } from 'src/types/shared/logger';
+import { Server } from 'http';
+import socket from 'socket.io';
 
 export interface SocketIOContract {
     logger: Logger;
+    httpServer: Server;
+    io: socket.Server;
 }
