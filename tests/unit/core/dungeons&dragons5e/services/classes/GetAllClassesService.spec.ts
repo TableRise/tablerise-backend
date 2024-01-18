5c926cc6cad57e3183c4f42d4dddb3a0627e82ad
Cesar Holanda <rasecmh0@gmail.com>
2024-01-18 16:00:17 -0300

    test: create unit tests for getAllClasses operation and service

---
+++ tests/unit/core/dungeons&dragons5e/services/classes/GetAllClassesService.spec.ts
@@ -0,0 +1,39 @@
+import sinon from 'sinon';
+import GetAllClassesService from 'src/core/dungeons&dragons5e/services/classes/GetAllClassesService';
+import DomainDataFaker from 'src/infra/datafakers/dungeons&dragons5e/DomainDataFaker';
+
+describe('Core :: Dungeons&dragons5e :: Service :: GetAllClassesService', () => {
+    let getAllClassesService: GetAllClassesService,
+        dungeonsAndDragonsRepository: any,
+        classes: any;
+
+    const logger = (): void => {};
+
+    context('When classes are recovered with success', () => {
+        before(() => {
+            classes = DomainDataFaker.generateDungeonsAndDragonsJSON({
+                count: 3,
+                entity: 'classes',
+            });
+
+            dungeonsAndDragonsRepository = {
+                find: sinon.spy(() => classes),
+                setEntity: sinon.spy(() => {}),
+            };
+
+            getAllClassesService = new GetAllClassesService({
+                dungeonsAndDragonsRepository,
+                logger,
+            });
+        });
+
+        it('should return the correct data and call correct methods', async () => {
+            const classesTest = await getAllClassesService.getAll();
+
+            expect(dungeonsAndDragonsRepository.setEntity).to.have.been.calledWith(
+                'Classes'
+            );
+            expect(classesTest).to.be.deep.equal(classes);
+        });
+    });
+});
