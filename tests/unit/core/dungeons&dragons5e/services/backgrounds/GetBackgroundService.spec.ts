a97d62e42a5469a1914730f8c57fc87e69cfd632
junglejf <junglejf@gmail.com>
2024-01-21 15:32:18 -0300

    test: create unit test for GetBackgroundOperation.ts GetDisabledBackgroundOperation.ts GetBackgroundService.ts GetDisabledBackgroundService.ts

---
+++ tests/unit/core/dungeons&dragons5e/services/backgrounds/GetBackgroundService.spec.ts
@@ -0,0 +1,39 @@
+import sinon from 'sinon';
+import GetBackgroundService from 'src/core/dungeons&dragons5e/services/backgrounds/GetBackgroundService';
+import DomainDataFaker from 'src/infra/datafakers/dungeons&dragons5e/DomainDataFaker';
+
+describe('Core :: Dungeons&dragons5e :: Service :: GetBackgroundService', () => {
+    let getBackgroundService: GetBackgroundService,
+        dungeonsAndDragonsRepository: any,
+        background: any;
+
+    const logger = (): void => {};
+
+    context('When background are recovered with success', () => {
+        before(() => {
+            background = DomainDataFaker.generateDungeonsAndDragonsJSON({
+                count: 1,
+                entity: 'backgrounds',
+            });
+
+            dungeonsAndDragonsRepository = {
+                findOne: sinon.spy(() => background),
+                setEntity: sinon.spy(() => {}),
+            };
+
+            getBackgroundService =
+                new GetBackgroundService({
+                    dungeonsAndDragonsRepository,
+                    logger,
+                });
+        });
+
+        it('should return the correct data and call correct methods', async () => {
+            
+            const backgroundTest = await getBackgroundService.get('ID_param');
+
+            expect(dungeonsAndDragonsRepository.setEntity).to.have.been.calledWith('Backgrounds');
+            expect(backgroundTest).to.be.deep.equal(background);
+        });
+    });
+});
