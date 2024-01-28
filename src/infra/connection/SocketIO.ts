cc5060e3f8df6504e5be722b972a7f8310f40417
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2024-01-28 00:51:31 -0300

    feat: Add types to socket io

--- src/infra/connection/SocketIO.ts
+++ src/infra/connection/SocketIO.ts
@@ -1,18 +1,19 @@
 import socket from 'socket.io';
 import { Server } from 'http';
 import InfraDependencies from 'src/types/modules/infra/InfraDependencies';
+import { SocketRooms } from 'src/types/modules/infra/connection/SocketIO';
 
 export default class SocketIO {
     private _socketInstance = {} as socket.Socket;
     private _io = {} as socket.Server;
-    private readonly _tables = {} as any;
+    private readonly _rooms = {} as SocketRooms;
     private readonly _logger;
 
     constructor({ logger }: InfraDependencies['socketIOContract']) {
         this._logger = logger;
 
         this.connect = this.connect.bind(this);
-        this._joinTableSocketEvent = this._joinTableSocketEvent.bind(this);
+        this._joinRoomSocketEvent = this._joinRoomSocketEvent.bind(this);
         this._changeBackgroundSocketEvent = this._changeBackgroundSocketEvent.bind(this);
         this._uploadImageSocketEvent = this._uploadImageSocketEvent.bind(this);
         this._disconnectSocketEvent = this._disconnectSocketEvent.bind(this);
@@ -30,42 +31,46 @@ export default class SocketIO {
 
         this._io.on('connection', (socket) => {
             this._socketInstance = socket;
-            socket.on('join', this._joinTableSocketEvent);
+            socket.on('join', this._joinRoomSocketEvent);
         });
     }
 
-    private async _joinTableSocketEvent(table: string): Promise<void> {
-        await this._socketInstance.join(table);
-        const tableData = this._tables[table as keyof typeof this._tables] || {
+    private async _joinRoomSocketEvent(tableId: string): Promise<void> {
+        await this._socketInstance.join(tableId);
+
+        const roomData = this._rooms[tableId] || {
             objects: [],
             images: [],
         };
+
         this._socketInstance.emit(
             'Joined at TableRise',
-            tableData.objects,
-            tableData.images
+            roomData.objects,
+            roomData.images
         );
     }
 
     private async _changeBackgroundSocketEvent(
-        table: string,
+        tableId: string,
         newBackground: string
     ): Promise<void> {
-        this._tables[table as keyof typeof this._tables].imagens.push(newBackground);
-        this._io.to(this._tables).emit('backgroundChanged', newBackground);
+        this._rooms[tableId].images.push(newBackground);
+        // Verificar linha abaixo com Isac, original: this._io.to(this._rooms).emit('backgroundChanged', newBackground);
+        this._io.to(this._rooms[tableId].images).emit('backgroundChanged', newBackground);
     }
 
     private async _uploadImageSocketEvent(
-        table: string,
+        tableId: string,
         squareId: string,
         imageData: string
     ): Promise<void> {
-        this._tables[table as keyof typeof this._tables].objects.map((object: any) =>
+        this._rooms[tableId].objects.map((object: any) =>
             object.elementID === parseInt(squareId)
                 ? { ...object, image: imageData }
                 : object
         );
-        this._io.to(this._tables).emit('updateObjectImage', squareId, imageData);
+        // Verificar linha abaixo com Isac, original: this._io.to(this._rooms).emit('updateObjectImage', squareId, imageData);
+        this._io.to(this._rooms[tableId].images).emit('updateObjectImage', squareId, imageData);
     }
 
     private async _disconnectSocketEvent(): Promise<void> {
--- src/types/modules/infra/connection/SocketIO.d.ts
+++ src/types/modules/infra/connection/SocketIO.d.ts
@@ -3,3 +3,10 @@ import { Logger } from 'src/types/shared/logger';
 export interface SocketIOContract {
     logger: Logger;
 }
+
+export interface SocketRoomInfo {
+    objects: any[];
+    images: any[];
+}
+
+export type SocketRooms = Record<string, SocketRoomInfo>;
