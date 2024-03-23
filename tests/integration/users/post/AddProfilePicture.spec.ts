91123fbcd3a31b6883d788a6f1b0a6a16c1904fa
Cesar Holanda <rasecmh0@gmail.com>
2024-03-23 19:43:23 -0300

    test: fix addProfilePicture attach

--- tests/integration/users/post/AddProfilePicture.spec.ts
+++ tests/integration/users/post/AddProfilePicture.spec.ts
@@ -1,5 +1,3 @@
-import fs from 'fs';
-import path from 'path';
 import { HttpStatusCode } from 'src/domains/common/helpers/HttpStatusCode';
 import { UserInstance } from 'src/domains/users/schemas/usersValidationSchema';
 import DomainDataFaker from 'src/infra/datafakers/users/DomainDataFaker';
@@ -7,7 +5,7 @@ import { InjectNewUser } from 'tests/support/dataInjector';
 import requester from 'tests/support/requester';
 
 describe('When a profile picture is uploaded', () => {
-    let user: UserInstance, file: any;
+    let user: UserInstance;
 
     before(async () => {
         user = DomainDataFaker.generateUsersJSON()[0];
@@ -16,27 +14,22 @@ describe('When a profile picture is uploaded', () => {
         user.picture = null;
 
         await InjectNewUser(user);
-
-        file = fs.createReadStream(
-            path.join(process.cwd(), '/tests/support/assets/test-image-batman.jpeg')
-        );
     });
 
     context('And all data is correct', () => {
         it('should return correct user with picture', async () => {
-            setTimeout(async () => {
-                const { body } = await requester()
-                    .post(`/profile/${user.userId}/update/picture`)
-                    .set('Content-Type', 'multipart/form-data')
-                    .attach('image', file)
-                    .expect(HttpStatusCode.OK);
+            const { body } = await requester()
+                .post(`/profile/${user.userId}/update/picture`)
+                .set('Content-Type', 'multipart/form-data')
+                .set('connection', 'keep-alive')
+                .attach('picture', './tests/support/assets/test-image-batman.jpeg')
+                .expect(HttpStatusCode.OK);
 
-                expect(body).to.have.property('createdAt');
-                expect(body).to.have.property('updatedAt');
-                expect(body.picture.id).to.be.equal('');
-                expect(body.picture.link).to.be.equal('');
-                expect(typeof body.picture.uploadDate).to.be.equal('string');
-            });
+            expect(body).to.have.property('createdAt');
+            expect(body).to.have.property('updatedAt');
+            expect(body.picture.id).to.be.equal('');
+            expect(body.picture.link).to.be.equal('');
+            expect(typeof body.picture.uploadDate).to.be.equal('string');
         });
     });
 });
