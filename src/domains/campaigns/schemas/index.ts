ef79911bc1224cc3115f27f97e2b3810af37bec0
Rafael Santos <rafaelcs_94@hotmail.com>
2024-04-21 20:20:34 -0300

    feat: set validation to match dates update

--- src/domains/campaigns/schemas/index.ts
+++ src/domains/campaigns/schemas/index.ts
@@ -1,9 +1,11 @@
 import campaignZod from './campaignsValidationSchema';
 import campaignUpdateZod from './campaignsUpdateValidationSchema';
+import campaignsUpdateMatchDatesZod from './campaignsUpdateMatchDatesValidationSchema';
 
 const schemas = {
     campaignZod,
     campaignUpdateZod,
+    campaignsUpdateMatchDatesZod,
 };
 
 export type SchemasCampaignType = typeof schemas;
