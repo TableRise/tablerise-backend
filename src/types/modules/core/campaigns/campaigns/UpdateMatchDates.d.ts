a004cc191a9f8c37ae623c4a93b3183a66dba1cd
Rafael Santos <rafaelcs_94@hotmail.com>
2024-04-21 20:21:25 -0300

    feat: update updateMatchDates interface

--- src/types/modules/core/campaigns/campaigns/UpdateMatchDates.d.ts
+++ src/types/modules/core/campaigns/campaigns/UpdateMatchDates.d.ts
@@ -1,9 +1,13 @@
 import UpdateMatchDatesService from 'src/core/campaigns/services/campaigns/UpdateMatchDatesService';
+import { SchemasCampaignType } from 'src/domains/campaigns/schemas';
+import SchemaValidator from 'src/domains/common/helpers/SchemaValidator';
 import CampaignsRepository from 'src/infra/repositories/campaign/CampaignsRepository';
 import { Logger } from 'src/types/shared/logger';
 
 export interface UpdateMatchDatesOperationContract {
     updateMatchDatesService: UpdateMatchDatesService;
+    campaignsSchema: SchemasCampaignType;
+    schemaValidator: SchemaValidator;
     logger: Logger;
 }
 
