63ffe7a8e9b6c23b9703ba90c7bdb195b3dafa3c
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-06-19 11:42:07 -0300

    feat: add validation schema to postInvitationEmailOperationContract contract

--- src/types/modules/core/campaigns/campaigns/PostInvitationEmail.ts
+++ src/types/modules/core/campaigns/campaigns/PostInvitationEmail.ts
@@ -1,10 +1,14 @@
 import PostInvitationEmailService from 'src/core/campaigns/services/PostInvitationEmailService';
+import { SchemasCampaignType } from 'src/domains/campaigns/schemas';
 import HttpRequestErrors from 'src/domains/common/helpers/HttpRequestErrors';
+import SchemaValidator from 'src/domains/common/helpers/SchemaValidator';
 import EmailSender from 'src/domains/users/helpers/EmailSender';
 import UsersRepository from 'src/infra/repositories/user/UsersRepository';
 import { Logger } from 'src/types/shared/logger';
 
 export interface postInvitationEmailOperationContract {
+    campaignsSchema: SchemasCampaignType;
+    schemaValidator: SchemaValidator;
     postInvitationEmailService: PostInvitationEmailService;
     logger: Logger;
 }
