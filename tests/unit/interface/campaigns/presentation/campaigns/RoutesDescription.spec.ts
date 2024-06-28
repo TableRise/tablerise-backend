7095cf438574cfef0607ce0d603e078e4b0d0f32
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-06-28 09:34:41 -0300

    fix: fix number of routes

--- tests/unit/interface/campaigns/presentation/campaigns/RoutesDescription.spec.ts
+++ tests/unit/interface/campaigns/presentation/campaigns/RoutesDescription.spec.ts
@@ -4,7 +4,7 @@ describe('Interface :: Campaigns :: Presentation :: RoutesDescription', () => {
     context('When desc object has all route descriptions', () => {
         it('should has the correct length', () => {
             const descLength = Object.keys(desc).length;
-            expect(descLength).to.be.equal(10);
+            expect(descLength).to.be.equal(11);
         });
     });
 });
