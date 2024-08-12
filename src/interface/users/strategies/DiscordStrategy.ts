cc0e26a7300d1edc9d437f87cedf8aab8ddf984c
Adson Gomes Oliveira <adsonprofessionalemail@gmail.com>
2024-08-11 21:03:38 -0300

    fix: Remove / from url

--- src/interface/users/strategies/DiscordStrategy.ts
+++ src/interface/users/strategies/DiscordStrategy.ts
@@ -10,7 +10,7 @@ passport.use(
             clientID: (DISCORD_CLIENT_ID as string) || 'default',
             clientSecret: (DISCORD_CLIENT_SECRET as string) || 'secret',
             scope: ['identify', 'email', 'guilds', 'guilds.join'],
-            callbackURL: `${process.env.SWAGGER_URL as string}/oauth/discord/callback`,
+            callbackURL: `${process.env.SWAGGER_URL as string}oauth/discord/callback`,
             passReqToCallback: true,
         },
         (request, accessToken, refreshToken, profile, done) => {
