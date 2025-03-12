f165902f3f076e6b3f05fd7f1f8ac31161afc6db
Adson Gomes Olivera <adsongoliveira2025@gmail.com>
2025-03-12 08:49:08 -0300

    fix: unit tests

--- tests/unit/interface/characters/presentation/characters/CharactersRoutes.spec.ts
+++ tests/unit/interface/characters/presentation/characters/CharactersRoutes.spec.ts
@@ -4,22 +4,25 @@ describe('Interface :: Characters :: Presentation :: Characters :: CharactersRou
     let charactersRoutes: CharactersRoutes,
         charactersController: any,
         verifyIdMiddleware: any,
+        authorizationMiddleware: any,
         imageMiddleware: any;
 
     context('When all the routes are correctly implemented', () => {
         charactersController = {};
         verifyIdMiddleware = () => ({});
         imageMiddleware = { multer: () => ({ single: () => {} }) };
+        authorizationMiddleware = { checkAdminRole: () => {} };
 
         charactersRoutes = new CharactersRoutes({
             charactersController,
             verifyIdMiddleware,
             imageMiddleware,
+            authorizationMiddleware
         });
 
         it('Should return the correct number of routes', () => {
             const routes = charactersRoutes.routes();
-            expect(routes).to.have.lengthOf(4);
+            expect(routes).to.have.lengthOf(5);
         });
     });
 });
